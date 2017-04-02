8cbbe74ac578b5f4e019a6239154d5f4c01534ce
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-02 18:15:31 +0200

    Add Calendar and first test

--- src/Calendar/Calendar.php
+++ src/Calendar/Calendar.php
@@ -2,7 +2,38 @@
 
 namespace CalendarReminder\Calendar;
 
-class Calender
-{
+use CalendarReminder\ConstantCollection;
 
+class Calendar
+{
+    /**
+     * @return array
+     */
+    public function months(): array
+    {
+        return [
+            '1' => 'January',
+            '2' => 'February',
+            '3' => 'March',
+            '4' => 'April',
+            '5' => 'Mai',
+            '6' => 'June',
+            '7' => 'July',
+            '8' => 'August',
+            '9' => 'September',
+            '10' => 'October',
+            '11' => 'November',
+            '12' => 'December'
+        ];
+    }
+
+    public function years(): array
+    {
+        $currentYear = (int) date('Y');
+
+        for ($i=$currentYear; $i < ConstantCollection::MAX_YEARS_GIVEN ; $i++) { 
+            $years[] = $i;
+        }
+        return $years;
+    }
 }
--- tests/Calendar/CalendarTest.php
+++ tests/Calendar/CalendarTest.php
@@ -4,15 +4,44 @@ namespace CalendarReminder\Calendar;
 
 use PHPUnit\Framework\TestCase;
 
-class CalenderTest extends TestCase
+use CalendarReminder\ConstantCollection;
+
+class CalendarTest extends TestCase
 {
     /**
      * @test
      */
-    public function itShouldHaveMonthAndYear()
+    public function itShouldHaveMonthAndYearsUpCurrentYear()
     {
         $calendar = new Calendar();
+
+        $monthArray = [
+            '1' => 'January',
+            '2' => 'February',
+            '3' => 'March',
+            '4' => 'April',
+            '5' => 'Mai',
+            '6' => 'June',
+            '7' => 'July',
+            '8' => 'August',
+            '9' => 'September',
+            '10' => 'October',
+            '11' => 'November',
+            '12' => 'December'
+        ];
+        $currentYear = date('Y');
+        $yearArray = [];
+
+        for ($i=$currentYear; $i < ConstantCollection::MAX_YEARS_GIVEN ; $i++) {
+            $yearArray[] = $i;
+        }
+
         $this->assertEquals($monthArray, $calendar->months());
         $this->assertEquals($yearArray, $calendar->years());
+    }
+
+    public function itShouldHaveCorrectDaysPerMonth()
+    {
+
     }
 }
