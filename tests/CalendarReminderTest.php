5b731ce46ea2a2f6c23d07712df56fad62c115b1
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-20 13:42:22 +0200

    Add itShouldAddReminders test

--- tests/CalendarReminderTest.php
+++ tests/CalendarReminderTest.php
@@ -6,5 +6,23 @@ use PHPUnit\Framework\TestCase;
 
 class CalendarReminderTest extends TestCase
 {
+    /** @var CalendarReminder */
+    private $calendarReminder;
 
+    protected function setUp()
+    {
+        $this->calendarReminder = new CalendarReminder();
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldAddReminders()
+    {
+        $date = date('d.m.y');
+        $content = 'some content';
+        $reminder = $this->calendarReminder->addReminder($date, $content);
+
+        $this->assertInternalType('array', $reminder[$date]);
+    }
 }
