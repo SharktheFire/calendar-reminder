f5bd10588d8cfbb0b9208e1a711b74af151199f4
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-20 13:42:22 +0200

    Add itShouldAddReminders test

--- src/CalendarReminder.php
+++ src/CalendarReminder.php
@@ -4,5 +4,20 @@ namespace CalendarReminder;
 
 class CalendarReminder
 {
+    /** @var array */
+    private $reminders;
 
+    /**
+     * @param string $date
+     * @param string $content
+     * @return array
+     */
+    public function addReminder(string $date, string $content): array
+    {
+        return $this->reminders = [
+            $date => [
+                $content
+            ]
+        ];
+    }
 }
