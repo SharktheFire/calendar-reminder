a99d46478dc983ee48d85d61f97c89de8c78d675
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-20 15:46:07 +0200

    Add ReminderFakeRepository for Tests

---
+++ tests/ReminderFakeRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CalendarReminder;
+
+class ReminderFakeRepository implements ReminderRepository
+{
+    /** @var Reminder[] */
+    public $reminders = [];
+
+    /** @var Report */
+    public $newReport;
+
+    /** @var bool */
+    public $saveMethodCalled = false;
+
+    /**
+     * @param string $date
+     * @param string $content
+     * @return Reminder
+     */
+    public function createReminder(string $date, string $content): Reminder
+    {
+        $reminder = new Reminder($date, $content);
+        $this->reminders[] = $reminder;
+        return $reminder;
+    }
+
+    /**
+     * @param Reminder $reminder
+     */
+    public function save(Reminder $reminder)
+    {
+        $this->reminders[] = $reminder;
+        $this->saveMethodCalled = true;
+    }
+}
