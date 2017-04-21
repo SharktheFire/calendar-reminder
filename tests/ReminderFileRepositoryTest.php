d2876d98af9464a5ea61eb23dc9c25e6b0e468c7
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-21 15:41:29 +0200

    Add createReminder Test

--- tests/ReminderFileRepositoryTest.php
+++ tests/ReminderFileRepositoryTest.php
@@ -29,8 +29,24 @@ class ReminderFileRepositoryTest extends TestCase
         $this->assertEquals(self::REMINDERS_ROOT_PATH, $this->reminderRepository->remindersPath());
     }
 
+
+    /**
+     * @test
+     */
     public function itShouldCreateReminderAndSave()
     {
+        $date = date('d.m.Y');
+        $content = 'some content';
+        $this->reminderRepository->createReminder($date, $content);
+        $reminderFileName = sprintf('%s/%s/%s'
+            , self::REMINDERS_ROOT_PATH
+            , $date
+            , $content
+        );
+        $foundReminder = unserialize(file_get_contents($reminderFileName));
+        $this->assertEquals($foundReminder->date(), $date);
+        $this->assertEquals($foundReminder->content(), $content);
+    }
 
     }
 }
