4e156b66784d84613c05faf3c5afd45650db46b7
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2017-04-21 15:40:57 +0200

    Add ensure Repository Root Path test

--- tests/ReminderFileRepositoryTest.php
+++ tests/ReminderFileRepositoryTest.php
@@ -6,12 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 class ReminderFileRepositoryTest extends TestCase
 {
+    const REMINDERS_ROOT_PATH = 'tests/reminders';
+
     /** @var ReminderFileRepository */
     public $reminderRepository;
 
     protected function setUp()
     {
-        $this->reminderRepository = new ReminderFileRepository();
+        $this->reminderRepository = new ReminderFileRepository(self::REMINDERS_ROOT_PATH);
+    }
+
+    protected function tearDown()
+    {
+        $this->deleteRecursive(self::REMINDERS_ROOT_PATH);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldHaveRepositoryRootPath()
+    {
+        $this->assertEquals(self::REMINDERS_ROOT_PATH, $this->reminderRepository->remindersPath());
     }
 
     public function itShouldCreateReminderAndSave()
